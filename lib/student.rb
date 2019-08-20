3af20ecd77bacb1bff4787a146e22a0676e300a4
Toni-Ty <antonique2479@yahoo.com>
2019-08-20 04:10:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,7 @@ def self.drop_table
 end
 
 def save
-  sql = <<-SQL 
+  sql = <<-SQL
     INSERT INTO students(name, grade)
     VALUES (?, ?)
   SQL
@@ -33,8 +33,8 @@ def save
   DB[:conn].execute(sql, self.name, self.grade)
   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 end
-  
-  
+
+
 
 
 
